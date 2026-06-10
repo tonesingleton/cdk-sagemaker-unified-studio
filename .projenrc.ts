ebd5d60f152691c471b61e2be63a66cb62b60a7d
Malik Alimoekhamedov <malik.alimoekhamedov@yuzzu.be>
2026-06-10 11:01:17 +0200

    chore: update aws-cdk-lib and prettier to latest versions

--- .projenrc.ts
+++ .projenrc.ts
@@ -6,7 +6,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   authorUrl: 'https://tonesingleton.com',
   authorAddress: 'https://tonesingleton.com',
   authorOrganization: true,
-  cdkVersion: '2.258.0',
+  cdkVersion: '2.258.1',
   defaultReleaseBranch: 'main',
   description: 'L2 CDK constructs for AWS SageMaker Unified Studio',
   jsiiVersion: '~5.9.44',
