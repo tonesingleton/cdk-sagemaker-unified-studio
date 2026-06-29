0ee3b01ad5e720b3b4ff66234673c4a364595623
Malik Alimoekhamedov <malik.alimoekhamedov@yuzzu.be>
2026-06-29 16:18:49 +0200

    feat: simplify environmentIdentifier handling in GlueConnection constructor

--- src/constructs/connection/glue/glue-connection.construct.ts
+++ src/constructs/connection/glue/glue-connection.construct.ts
@@ -1,7 +1,6 @@
 import { aws_datazone as datazone } from 'aws-cdk-lib';
 import type { Construct } from 'constructs';
 import { GlueComputeEnvironment, type GlueConnectionProps } from './glue-connection.interface';
-import { Environment } from '../../environment/environment.construct';
 
 const ALL_COMPUTE_ENVIRONMENTS = [
   GlueComputeEnvironment.SPARK,
@@ -19,11 +18,7 @@ const ALL_COMPUTE_ENVIRONMENTS = [
  */
 export class GlueConnection extends datazone.CfnConnection {
   constructor(scope: Construct, id: string, props: GlueConnectionProps) {
-    const environmentIdentifier =
-      props.environmentIdentifier ??
-      scope.node.scopes.reverse().find((s): s is Environment => s instanceof Environment)?.environmentId;
-
-    if (!environmentIdentifier) {
+    if (!props.environmentIdentifier) {
       throw new Error(
         'environmentIdentifier is required for GlueConnection. Provide it explicitly or place the construct inside an Environment.',
       );
@@ -54,7 +49,7 @@ export class GlueConnection extends datazone.CfnConnection {
     super(scope, id, {
       domainIdentifier: props.domainIdentifier,
       projectIdentifier: props.projectIdentifier,
-      environmentIdentifier,
+      environmentIdentifier: props.environmentIdentifier,
       name: props.name,
       description: props.description,
       awsLocation: props.awsLocation,
